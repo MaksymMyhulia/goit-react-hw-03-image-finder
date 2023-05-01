929921ef4ba12d9d7521853ab6536a259ed5b004
Maksym Myhulia <113797631+MaksymMyhulia@users.noreply.github.com>
2023-05-01 21:53:36 +0300

    add_getImages.js

---
+++ src/services/getImages.js
@@ -0,0 +1,23 @@
+const BASE_URL = `https://pixabay.com/api/`;
+const API_KEY = '34429700-a5059cc9453f6d8a6aa28d125';
+
+const getImages = (searchText, page = 1) => {
+  return fetch(
+    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${searchText}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(
+      new Error(
+        `Oops... there are no ${searchText} images matching your search... `
+      )
+    );
+  });
+};
+
+const api = {
+  getImages,
+};
+
+export default api;
