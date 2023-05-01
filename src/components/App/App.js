154d7ece46331ef3c27781aca15b7d184666f002
Maksym Myhulia <113797631+MaksymMyhulia@users.noreply.github.com>
2023-05-01 21:40:20 +0300

    add_App

---
+++ src/components/App/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import { GlobalStyle } from "./GlobalStyle";
+import ImageGallery from "components/ImageGallery/ImageGallery"
+import { Searchbar } from "components/Searchbar/Searchbar";
+
+export default class App extends Component {
+state = {
+  textSearch: '',
+}
+
+handleSubmit = textSearch => {
+  this.setState({ textSearch });
+};
+
+render() {
+  const { textSearch } = this.state;
+
+  return (
+    <>
+      <Searchbar onSubmit={this.handleSubmit} />
+      <ImageGallery value={textSearch} />
+      <GlobalStyle />
+    </>
+  )
+}
+}
